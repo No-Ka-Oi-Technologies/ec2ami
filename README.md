261fee013b487e52868d870dd6d60351bad585f8
chet rungruang <chetster@gmail.com>
2024-04-10 11:51:32 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# ec2ami.sh
+
+Simple bash script for creation of AMIs from your RUNNING EC2 Instances without rebooting the instances.
+
+## Requirements
+
+- AWSCLI installed & configured
+  - If you have multiple AWS accounts, ensure to properly `export AWS_PROFILE=<name_of_account>`
+
+- `jq` for parsing json
+
+## Limitations
+
+- Instances with `tag.Name` that include a space will be truncated due to restrictions with AMI naming
+
+- AMI names are immutable so you cannot have more than one with the same name (i.e. this script isn't meant to be run multiple times a day unless you want to include a timestamp in the AMI name)
---
+++ ec2ami.sh
@@ -0,0 +1,22 @@
+# Get a list of all running EC2 instances
+instance_ids=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].InstanceId' --output json)
+
+# Set current date for naming AMI
+current_date=$(date +"%Y%m%d")
+
+# Loop through each instance and create an AMI without rebooting
+for instance_id in $(echo "${instance_ids}" | jq -r '.[]'); do
+    # Get the description of the instance
+    description=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[].Instances[].Tags[?Key==`Name`].Value' --output text)
+    
+    # Truncate the description at the first space
+    ami_name=$(echo "$current_date-$description" | awk '{print $1}')
+    
+    # Output instance ID and AMI name
+    echo "Instance ID: $instance_id, Creating AMI Name: $ami_name"
+    
+    # Create the AMI without rebooting and suppress standard output
+    if ! aws ec2 create-image --instance-id $instance_id --name "$ami_name" --no-reboot > /dev/null; then
+        echo "Error creating AMI for instance: $instance_id" >&2
+    fi
+done
